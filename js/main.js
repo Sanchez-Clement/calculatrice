ebd646c37d45a9331a0a6115a49961ba141c0b5f
Clement Sanchez <sanchez.clmnt@gmail.com>
2017-07-10 16:26:13 +0200

    make stytle of calcultrice

---
+++ js/main.js
@@ -0,0 +1,76 @@
+var chiffre = "";
+var total = "";
+var final = [];
+var test = [];
+var result = 0;
+
+function calculate(chiffre) {
+
+  if (chiffre == "reset") {
+    total = "";
+    final = [];
+    chiffre = "";
+    document.getElementById("total").innerHTML = total;
+
+  } else if (chiffre == "C") {
+    test = total.split("");
+    test.pop();
+
+    total = test.join("");
+
+    document.getElementById("total").innerHTML = total;
+
+  } else if (chiffre == "=") {
+    final.push(total);
+    // document.getElementById("total").innerHTML = final;
+    // console.log
+    // console.log(final);
+  } else if (chiffre == "-" || chiffre == "+" || chiffre == "/" || chiffre == "x") {
+    if (final.length < 1) {
+      final.push(total);
+      total = "";
+      final.push(chiffre);
+      console.log(final);
+      document.getElementById("total").innerHTML = "";
+    } else {
+
+
+      total = "";
+      final.push(chiffre);
+      console.log(final);
+      document.getElementById("total").innerHTML = "";
+
+
+    }
+
+  } else {
+    total += chiffre;
+
+
+    document.getElementById("total").innerHTML = total;
+
+  }
+
+  if (final.length == 3) {
+
+    if (final[1] == "-") {
+      result = Number(final[0]) - Number(final[2]);
+    } else if (final[1] == "x") {
+      result = Number(final[0]) * Number(final[2]);
+    } else if (final[1] == "/") {
+      result = Number(final[0]) / Number(final[2]);
+    } else {
+      result = Number(final[0]) + Number(final[2]);
+    }
+
+    // result = Number(final[0]) + final[1] + Number(final[2]);
+    // console.log(result);
+    document.getElementById("total").innerHTML = result;
+    final = [result];
+    chiffre = "";
+    total = "";
+    console.log(final);
+
+  }
+
+}
